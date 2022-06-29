c1c07af08ad1cbe2b4a0556960338eb751d4656f
Igor Barbashov <bis200705@gmail.com>
2022-06-29 11:43:53 +0300

    04_variables

--- lesson_01/01_hello_world.cmd
+++
@@ -1,8 +0,0 @@
-# preprocessing
-gcc -E 01_hello_world.c > 01_hello_world_preprocessing.c
-
-# compilation
-gcc -c 01_hello_world_preprocessing.c
-
-# linking, make executable file
-gcc 01_hello_world_preprocessing.o -o hello.exe
---
+++ lesson_01/04_declarations.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <inttypes.h>
+
+int main(int argc, char* argv[])
+{
+    char c;
+    signed char sc;
+    unsigned char uc;
+
+    int i;
+    signed int si; // default
+    unsigned int ui;
+    long int li;
+    long long int lli;
+    short int si;
+
+    int8_t i8; // byte
+    int16_t i16; // 2 bytes
+    int32_t i32; // 4 bytes
+    int64_t i64; // 8 bytes
+
+    uint8_t i8; // unsigned byte
+    uint16_t i16; // unsigned 2 bytes
+    uint32_t i32; // unsigned 4 bytes
+    uint64_t i64; // unsigned 8 bytes
+
+    double d;
+    bool b;
+
+    return 0;
+}
